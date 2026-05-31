9361cce475e996af2e88a61bab59ce0a116c36bb
Isaac Fayah <2425401588@live.gctu.edu.gh>
2026-05-31 01:31:08 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Isaac Fayah
+# STUDENT ID:2425401588
+# Email: ifayah25@icloud.com
+# Grader: Augustus Buckman
+#
+# On my honor, Isaac Fayah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
-
+    print()
+    print('...IKF')
+    print()
+    print('...IIIIIIIIIIII........KKKK....KKKK........FFFFFFFFFFFF...')
+    print('...IIIIIIIIIIII........KKKK....KKKK........FFFFFFFFFFFF...')
+    print('......IIIIII...........KKKK....KKKK........FFFFF..........')
+    print('......IIIIII...........KKKK....KKKK........FFFFFFFFFFFF...')
+    print('......IIIIII...........KKKKKKKKKK..........FFFFFFFFFFFF...')
+    print('......IIIIII...........KKKKKKKKKK..........FFFFF..........')
+    print('......IIIIII...........KKKK....KKKK........FFFFF..........')
+    print('......IIIIII...........KKKK....KKKK........FFFFF..........')
+    print('...IIIIIIIIIIII...**...KKKK....KKKK...**...FFFFF..........')
+    print('...IIIIIIIIIIII...**...KKKK....KKKK...**...FFFFF..........')
+    print()
 main()
